e10be98866c09f033eb862718627c6bf9e6e3298
Livelyoneweek <wlvvwhs@naver.com>
2022-10-30 23:55:32 +0900

    221030 @Aspect프록시-설명

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -8,6 +8,9 @@ import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -22,7 +25,10 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ProxyFactoryConfigV1.class)
-@Import(ProxyFactoryConfigV2.class)
+//@Import(ProxyFactoryConfigV2.class)
+//@Import(BeanPostProcessorConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의, Imnport 통해서 AppV1Config만 빈 등록하고 싶어서 해놓은 것임 예제용!!!
 public class ProxyApplication {
 
---
+++ src/main/java/hello/proxy/config/v6_aop/aspect/LogTraceAspect.java
@@ -0,0 +1,40 @@
+package hello.proxy.config.v6_aop.aspect;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect // 어드바이저 생성지원을 해줌, aop 빌드 해줘야 사용가능
+public class LogTraceAspect {
+
+    private final LogTrace logTrace;
+
+    public LogTraceAspect(LogTrace logTrace) {
+        this.logTrace = logTrace;
+    }
+
+    @Around("execution(* hello.proxy.app..*(..))") // @Around 는 포인트컷 조건
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        TraceStatus status = null;
+        // log.info("target={}", joinPoint.getTarget()); //실제 호출 대상
+        // log.info("getArgs={}", joinPoint.getArgs()); //전달인자
+        // log.info("getSignature={}", joinPoint.getSignature()); //join point 시그니처
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+            //로직 호출
+            Object result = joinPoint.proceed();
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
